b9b24bf910f8eff594ce0f76405027ce1375dcde
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-10-26 15:27:08 +0100

    Implemented dot product calculation with loop

--- lab1_matrix_multiplication/matmul.py
+++ lab1_matrix_multiplication/matmul.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 
+def dot(vec1: np.ndarray, vec2: np.ndarray) \
+        -> float:
+    """
+    Calculates the dot product between vectors.
+
+    :param vec1: vector of shape (n,)
+    :param vec2: vector of shape (n,)
+    :return: dot product of vectors as a single float number
+    """
+    dot_product = 0
+    for i in range(vec1.shape[0]):
+        dot_product += vec1[i] * vec2[i]
+    return dot_product
+
+
 def matmul_1a(A: np.ndarray, B: np.ndarray) -> np.ndarray:
     """
     IJP matrix multiplication, using dot product between rows of A and
@@ -18,7 +33,7 @@ def matmul_1a(A: np.ndarray, B: np.ndarray) -> np.ndarray:
     for i in range(m):
         for j in range(n):
             # slice 0:k is mathematical range [0, k-1]
-            C[i, j] += np.dot(A[i, 0:k], B[0:k, j])
+            C[i, j] += dot(A[i, 0:k], B[0:k, j].flatten())
 
     return C
 
