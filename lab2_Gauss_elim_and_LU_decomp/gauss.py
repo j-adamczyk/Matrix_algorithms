c9d17d197842dbd5c0e1d750d99447e90999467a
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2020-11-07 22:38:22 +0100

    Gauss and LU decomposition done

---
+++ lab2_Gauss_elim_and_LU_decomp/gauss.py
@@ -0,0 +1,72 @@
+import numpy as np
+from scipy.linalg import lu_factor
+
+from lab2_Gauss_elim_and_LU_decomp.pivoting import pivoting
+
+
+def gauss_elimination_classic(A: np.ndarray) \
+        -> np.ndarray:
+    """
+    Performs Gaussian elimination on matrix A in the classic version
+    with pivoting.
+
+    :param A: square matrix of shape (n, n)
+    :return: matrix A after Gaussian elimination
+    """
+    A = A.copy()
+    n = A.shape[0]
+    for k in range(n):
+        A = pivoting(A, k, "row")
+        Akk = A[k, k]
+        for j in range(k + 1, n):
+            A[j, k:n] -= A[k, k:n] * A[j, k] / Akk
+
+    return A
+
+
+def gauss_elimination_row(A: np.ndarray) \
+        -> np.ndarray:
+    """
+    Performs Gaussian elimination on matrix A row-wise with pivoting.
+
+    :param A: square matrix of shape (n, n)
+    :return: matrix A after Gaussian elimination
+    """
+    A = A.copy()
+    n = A.shape[0]
+    for k in range(n):
+        A = pivoting(A, k, "row")
+        A[k, k:] = A[k, k:] / A[k, k]
+        for j in range(k + 1, n):
+            A[j, k:] -= A[k, k:] * A[j, k]
+    return A
+
+
+def gauss_elimination_column(A: np.ndarray) \
+        -> np.ndarray:
+    """
+    Performs Gaussian elimination on matrix A column-wise with pivoting.
+
+    :param A: square matrix of shape (n, n)
+    :return: matrix A after Gaussian elimination
+    """
+    A = A.copy()
+    n = A.shape[0]
+    for k in range(n):
+        A = pivoting(A, k, "col")
+        A[k + 1:n, k] = A[k + 1:n, k] / A[k, k]
+        for j in range(k + 1, n):
+            A[k + 1:n, j] -= A[k + 1:n, k] * A[k, j]
+
+    return A
+
+
+A = np.array([[1, 1, 1],
+              [2, 3, 7],
+              [1, 2, 3]], dtype=np.float32)
+
+A_custom = gauss_elimination_column(A.copy())
+A_builtin = lu_factor(A)[0]
+print(A_custom)
+print()
+print(A_builtin)
---
+++ lab2_Gauss_elim_and_LU_decomp/lu.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from lab2_Gauss_elim_and_LU_decomp.pivoting import pivoting
+
+
+def LU_decomposition(A: np.ndarray) \
+        -> np.ndarray:
+    """
+    Performs LU decomposition on matrix on A with pivoting, saving results in
+    a new matrix.
+
+    :param A: square matrix of shape (n, n)
+    :return: matrix with L and U matrices
+    """
+    A = A.copy()
+    n = A.shape[0]
+    L = np.zeros(A.shape)
+    U = np.zeros(A.shape)
+    for k in range(n):
+        A = pivoting(A, k, version="row")
+        L[k, k] = 1
+        L[k + 1:, k] = A[k + 1:, k] / A[k, k]
+        U[k, k:] = A[k, k:]
+        A[k + 1:, k] = 0
+        for j in range(k + 1, n):
+            A[k + 1:, j] -= L[k + 1:, k] * U[k, j]
+    return A
